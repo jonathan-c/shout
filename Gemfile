9eefe020f9bc82cebbd86c90adb296dcb2c788cc
Jonathan Castillo <jonathan@ygm.com>
2013-05-07 23:35:55 -0400

    moved over my work

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'sqlite3'
+gem 'strong_parameters', '0.2.0'
+gem 'monban', '0.0.6'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '96dbc56fc0947a7bde0145c2072b6dbe7e595f69e2214c4d9d9594a40b048f1ca5f3de5bc6069ee7e6cef8f1b8ffbab506c044e118b8d454d4bb723a65edf3ac'
