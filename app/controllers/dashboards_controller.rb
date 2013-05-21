1fc27d44aec257bd40e21467f071f45b03ed7085
Jonathan Castillo <joncastillo123@gmail.com>
2013-05-20 21:41:10 -0400

    refactored

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,9 @@
 class DashboardsController < ApplicationController
   
   def show
-    @text_shout = TextShout.new
-    @photo_shout = PhotoShout.new
-    @shouts = current_user.shouts
-    puts @shouts.map(&:content)
+    # @text_shout = TextShout.new
+    # @photo_shout = PhotoShout.new
+    # @shouts = current_user.shouts
+    @dashboard = Dashboard.new(current_user)
   end
 end
